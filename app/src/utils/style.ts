f57f5d87070c7380d693d28e4de7a27f73b93544
oberruti <54275024+oberruti@users.noreply.github.com>
2021-01-15 14:03:42 -0300

    adding common styles reutilizable

---
+++ app/src/utils/style.ts
@@ -0,0 +1,10 @@
+export const commonStyleResources = {
+    colors: {
+        yellow: '#f4b100',
+        gray: '#313131' 
+    },
+    dimensions: {
+        backgroundHeight: '720px',
+        backgroundWidth: '1080px',
+    }
+}
