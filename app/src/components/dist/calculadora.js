1a1668165dec9be000df4c425b0316bf2de8c321
oberruti <54275024+oberruti@users.noreply.github.com>
2021-01-15 14:04:02 -0300

    style of the calculator added

--- app/src/components/dist/calculadora.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-function Calculadora() {
-    return (react_1["default"].createElement("div", null, "Hola"));
-}
-exports["default"] = Calculadora;
